91cec5d8078afda3db7da597cd346e5daf329a79
vaplev <vaplev@mail.ru>
2022-04-29 16:05:54 +0300

    Test try to use network (without resize)

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-import imp
+from ast import Global
 import time
+import random
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import Dataset, DataLoader
+
 from torchvision import datasets
 from torchvision.transforms import transforms
 from torch.autograd import Variable
-from skimage import io
+from skimage import io, color
+from skimage.transform import resize
+from torch.utils.data import Dataset, DataLoader
 import numpy as np
 import matplotlib.pyplot as plt
 import os
@@ -18,11 +21,11 @@ class Dense_Block(nn.Module):
         self.relu = nn.ReLU(inplace = True)
         self.bn = nn.BatchNorm2d(num_features = in_channels)
     
-        self.conv1 = nn.Conv2d(in_channels = in_channels, out_channels = 32, kernel_size = 3, stride = 1, padding = 1)
-        self.conv2 = nn.Conv2d(in_channels = 32, out_channels = 32, kernel_size = 3, stride = 1, padding = 1)
-        self.conv3 = nn.Conv2d(in_channels = 64, out_channels = 32, kernel_size = 3, stride = 1, padding = 1)
-        self.conv4 = nn.Conv2d(in_channels = 96, out_channels = 32, kernel_size = 3, stride = 1, padding = 1)
-        self.conv5 = nn.Conv2d(in_channels = 128, out_channels = 32, kernel_size = 3, stride = 1, padding = 1)
+        self.conv1 = nn.Conv2d(in_channels = in_channels, out_channels = 4, kernel_size = 3, stride = 1, padding = 1)
+        self.conv2 = nn.Conv2d(in_channels = 4, out_channels = 4, kernel_size = 3, stride = 1, padding = 1)
+        self.conv3 = nn.Conv2d(in_channels = 8, out_channels = 4, kernel_size = 3, stride = 1, padding = 1)
+        self.conv4 = nn.Conv2d(in_channels = 12, out_channels = 4, kernel_size = 3, stride = 1, padding = 1)
+        self.conv5 = nn.Conv2d(in_channels = 16, out_channels = 4, kernel_size = 3, stride = 1, padding = 1)
     def forward(self, x):
         bn = self.bn(x)
         conv1 = self.relu(self.conv1(bn))
@@ -46,30 +49,47 @@ class Transition_Layer(nn.Module):
     
         self.relu = nn.ReLU(inplace = True) 
         self.bn = nn.BatchNorm2d(num_features = out_channels) 
-        self.conv = nn.Conv2d(in_channels = in_channels, out_channels = out_channels, kernel_size = 1, bias = False) 
-        self.avg_pool = nn.AvgPool2d(kernel_size = 2, stride = 2, padding = 0)
+        self.conv = nn.Conv2d(in_channels = in_channels, out_channels = out_channels, kernel_size = 3, stride = 1, padding = 1, bias = False) 
+        #self.avg_pool = nn.AvgPool2d(kernel_size = 2, stride = 2, padding = 0)
     def forward(self, x): 
-        bn = self.bn(self.relu(self.conv(x))) 
-        out = self.avg_pool(bn) 
+        out = self.bn(self.relu(self.conv(x))) 
+        #out = self.avg_pool(bn) 
+        return out
+
+class UpTransition_Layer(nn.Module): 
+    def __init__(self, in_channels, out_channels):
+        super(UpTransition_Layer, self).__init__() 
+    
+        self.relu = nn.ReLU(inplace = True) 
+        self.bn = nn.BatchNorm2d(num_features = out_channels) 
+        #self.conv = nn.ConvTranspose2d(in_channels = in_channels, out_channels = out_channels, kernel_size = 2, stride = 2, bias = False)
+        self.conv = nn.Conv2d(in_channels = in_channels, out_channels = out_channels, kernel_size = 3, stride = 1, padding = 1, bias = False)   
+    def forward(self, x): 
+        out = self.bn(self.relu(self.conv(x))) 
         return out
 
 class DenseNet(nn.Module): 
     def __init__(self, nr_classes): 
         super(DenseNet, self).__init__() 
   
-        self.lowconv = nn.Conv2d(in_channels = 3, out_channels = 64, kernel_size = 7, padding = 3, bias = False) 
+        self.lowconv = nn.Conv2d(in_channels = 3, out_channels = 8, kernel_size = 7, padding = 3, bias = False) 
         self.relu = nn.ReLU()
     
         # Make Dense Blocks 
-        self.denseblock1 = self._make_dense_block(Dense_Block, 64) 
-        self.denseblock2 = self._make_dense_block(Dense_Block, 128)
-        self.denseblock3 = self._make_dense_block(Dense_Block, 128)
+        self.denseblock1 = self._make_dense_block(Dense_Block, 8) 
+        self.denseblock2 = self._make_dense_block(Dense_Block, 16)
+        self.denseblock3 = self._make_dense_block(Dense_Block, 16)
         # Make transition Layers 
-        self.transitionLayer1 = self._make_transition_layer(Transition_Layer, in_channels = 160, out_channels = 128) 
-        self.transitionLayer2 = self._make_transition_layer(Transition_Layer, in_channels = 160, out_channels = 128) 
-        self.transitionLayer3 = self._make_transition_layer(Transition_Layer, in_channels = 160, out_channels = 64)
-        # Classifier 
-        self.bn = nn.BatchNorm2d(num_features = 64) 
+        self.transitionLayer1 = self._make_transition_layer(Transition_Layer, in_channels = 20, out_channels = 16) 
+        self.transitionLayer2 = self._make_transition_layer(Transition_Layer, in_channels = 20, out_channels = 16) 
+        self.transitionLayer3 = self._make_transition_layer(Transition_Layer, in_channels = 20, out_channels = 8)
+        # Upsampling transition layers
+        self.uptransitionLayer1 = self._make_transition_layer(UpTransition_Layer, in_channels = 8, out_channels = 16) 
+        self.uptransitionLayer2 = self._make_transition_layer(UpTransition_Layer, in_channels = 16, out_channels = 20) 
+        self.uptransitionLayer3 = self._make_transition_layer(UpTransition_Layer, in_channels = 20, out_channels = 3)
+        # Classifier
+        self.sigm = nn.Sigmoid()
+        self.bn = nn.BatchNorm2d(num_features = 8) 
         self.pre_classifier = nn.Linear(64*4*4, 512) 
         self.classifier = nn.Linear(512, nr_classes)
  
@@ -81,68 +101,48 @@ class DenseNet(nn.Module):
         modules = [] 
         modules.append(layer(in_channels, out_channels)) 
         return nn.Sequential(*modules)
-    def forward(self, x): 
+    def forward(self, x):
+        #print(x.shape) 
         out = self.relu(self.lowconv(x))
+        #print(out.shape)
 
-        out = self.denseblock1(out) 
+        out = self.denseblock1(out)
+        #print(out.shape) 
         out = self.transitionLayer1(out)
+        #print(out.shape)
 
         out = self.denseblock2(out) 
-        out = self.transitionLayer2(out) 
+        #print(out.shape)
+        out = self.transitionLayer2(out)
+        #print(out.shape) 
     
         out = self.denseblock3(out) 
+        #print(out.shape)
         out = self.transitionLayer3(out) 
+        #print(out.shape)
  
-        out = self.bn(out) 
-        out = out.view(-1, 64*4*4) 
+        out = self.bn(out)
+        #print(out.shape)
+
+        out = self.uptransitionLayer1(out)
+        #print(out.shape)
+        out = self.uptransitionLayer2(out)
+        #print(out.shape)
+        out = self.uptransitionLayer3(out)
+        #print(out.shape)
+        # out = out.view(-1, 64*4*4) 
     
-        out = self.pre_classifier(out) 
-        out = self.classifier(out)
-        return out
-
-# Классы нейронной сети
-# Первый реализует дескриминатор, задача которого в том, чтобы отличить
-# сгенерированное изображение от реального.
-# Второй реализует генератор, задача которого в генерации изображений, получая
-# на вход шум
-class discriminator(nn.Module):
-    def __init__(self):
-        super(discriminator, self).__init__()
-        self.label_emb = nn.Embedding(10, 10)
-        self.fc1 = nn.Linear(794, 512)
-        self.fc2 = nn.Linear(512, 1)
-        self.activation = nn.LeakyReLU(0.1)
-    def forward(self, x, labels):
-        x = x.view(-1, 784)
-        y = self.label_emb(labels)
-        x = torch.cat([x, y], 1)
-        x = self.activation(self.fc1(x))
-        x = self.fc2(x)
-        return nn.Sigmoid()(x)
-class generator(nn.Module):
-    def __init__(self):
-        super(generator, self).__init__()
-        self.label_emb = nn.Embedding(10, 10)
-        self.fc1 = nn.Linear(138, 1024)
-        self.fc2 = nn.Linear(1024, 2048)
-        self.fc3 = nn.Linear(2048, 784)
-        self.activation = nn.ReLU()
-    def forward(self, x, labels):
-        x.view(-1, 128)
-        y = self.label_emb(labels)
-        x = torch.cat([x, y], 1)
-        x = self.activation(self.fc1(x))
-        x = self.activation(self.fc2(x))
-        x = self.fc3(x)
-        x = x.view(-1, 1, 28, 28)
-        return nn.Tanh()(x)
-
+        # out = self.pre_classifier(out) 
+        # out = self.classifier(out)
+        return self.sigm(out)
 
 class ResImageDataset(Dataset):
     def __init__(self, dir_name, transform=None):
         self.dir_name = dir_name
         self.raw = os.listdir(os.path.join(dir_name, "raw"))
         self.inpainted = os.listdir(os.path.join(dir_name, "inpainted"))
+        print("raw", self.raw)
+        print("inpainted", self.inpainted)
         self.transform = transform
     
     def __len__(self):
@@ -153,134 +153,137 @@ class ResImageDataset(Dataset):
             index = index.tolist()
         
         raw_image = io.imread(os.path.join(self.dir_name, "raw", self.raw[index]))
-        inpainted_images = io.imread(os.path.join(self.dir_name, "inpainted", self.inpainted[index]))
+        inpainted_image = io.imread(os.path.join(self.dir_name, "inpainted", self.inpainted[index]))
 
-        sample = {"raw": raw_image, "inpainted": inpainted_images}
+        sample = None
+
+        if len(raw_image.shape) < 3:
+            raw_image = color.gray2rgb(raw_image)
+        if len(inpainted_image.shape) < 3:
+            inpainted_image = color.gray2rgb(inpainted_image)
+
+        if raw_image.shape != inpainted_image.shape:
+            print("Image {} size problem".format(index))
+            print(raw_image.shape)
+            raw_image = resize(raw_image, (inpainted_image.shape[0], inpainted_image.shape[1]))
 
         if self.transform:
-            sample = self.transform(sample)
+            sample = {"raw": self.transform(raw_image),
+                      "inpainted": self.transform(inpainted_image)}
+        else:
+            sample = {"raw": raw_image,
+                      "inpainted": inpainted_image}
 
         return sample
 
 
 print("Is cuda possible = ", torch.cuda.is_available())
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+# device = torch.device('cpu')
 
 # Гиперпараметры
-epochs = 100
-lr = 2e-4
-batch_size = 32
+epochs = 10
+lr = 1e-2
+batch_size = 1
 # Логистическая функция потерь (бинарная перекрестная энтропия) 
 loss = nn.BCELoss()
 
 # Создание моделей и передача их на устройство (в нашем случае - видеокарта)
-G = generator().to(device)
-D = discriminator().to(device)
+G = DenseNet(6).to(device)
 
-G_optimizer = optim.Adam(G.parameters(), lr=lr, betas=(0.5, 0.999))
-D_optimizer = optim.Adam(D.parameters(), lr=lr, betas=(0.5, 0.999))
+G_optimizer = optim.Adam(G.parameters(), lr=lr)
 
 # Создание преобразователя изображений из набора MNIST
-transform = transforms.Compose([transforms.ToTensor(),
-                                transforms.Normalize((0.5,), (0.5,))])
-# Создаем итерируемый загрузчик данных из MNIST
-train_set = datasets.MNIST('mnist/', train=True, download=True, transform=transform)
+transform = transforms.Compose([transforms.ToTensor()])
+
+train_set = ResImageDataset("Images", transform=transform)
 train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
 
-def train():
-    # Обучение производим эпохами, количество которых задается гиперпараметром
-        for epoch in range(epochs):
-            for idx, (imgs, labels) in enumerate(train_loader):
-                labels = labels.to(device)
-                idx += 1
-                # Обучаем дискриминатор
-                # real_inputs - изображения из набора данных MNIST 
-                # fake_inputs - изображения от генератора
-                # real_inputs должны быть классифицированы как 1, а fake_inputs - как 0
-                real_inputs = imgs.to(device)
-                real_outputs = D(real_inputs, labels)
-                real_label = torch.ones(real_inputs.shape[0], 1).to(device)
-                noise = (torch.rand(real_inputs.shape[0], 128) - 0.5) / 0.5
-                noise = noise.to(device)
+def test_train():
+    for epoch in range(1, epochs + 1):
+        # monitor training loss
+        train_loss = 0.0
 
-                fake_label_to_generate = torch.LongTensor(np.random.randint(0, 10, batch_size)).to(device)
-
-                fake_inputs = G(noise, fake_label_to_generate)
-                fake_outputs = D(fake_inputs, fake_label_to_generate)
-                fake_label = torch.zeros(fake_inputs.shape[0], 1).to(device)
-                outputs = torch.cat((real_outputs, fake_outputs), 0)
-                targets = torch.cat((real_label, fake_label), 0)
-                D_loss = loss(outputs, targets)
-                D_optimizer.zero_grad()
-                D_loss.backward()
-                D_optimizer.step()
-                # Обучаем генератор
-                # Цель генератора получить от дискриминатора 1 по всем изображениям
-                noise = (torch.rand(real_inputs.shape[0], 128)-0.5)/0.5
-                noise = noise.to(device)
-
-                fake_label_to_generate = torch.LongTensor(np.random.randint(0, 10, batch_size)).to(device)
-
-                fake_inputs = G(noise, fake_label_to_generate)
-                fake_outputs = D(fake_inputs, fake_label_to_generate)
-                fake_targets = torch.ones([fake_inputs.shape[0], 1]).to(device)
-                G_loss = loss(fake_outputs, fake_targets)
-                G_optimizer.zero_grad()
-                G_loss.backward()
-                G_optimizer.step()
-                if idx % 100 == 0 or idx == len(train_loader):
-                    print('Epoch {} Iteration {}: discriminator_loss {:.3f} generator_loss {:.3f}'.format(epoch, idx, D_loss.item(), G_loss.item()))
-        torch.save(G, 'Generator.pth')
-        print('Model saved.')
+        #Training
+        for i, sample in enumerate(train_loader):
+            raw = sample["raw"]
+            #print(raw.shape)
+            shape = raw.shape
+            raw = raw.view(1, shape[1], shape[2], shape[3]).to(device, dtype = torch.float)
+            inpainted = sample["inpainted"]
+            shape = inpainted.shape
+            inpainted = inpainted.view(1, shape[1], shape[2], shape[3]).to(device, dtype = torch.float)
+            G_optimizer.zero_grad()
+            outputs = G(raw)
+            cur_loss = loss(outputs, inpainted)
+            cur_loss.backward()
+            G_optimizer.step()
+            train_loss += cur_loss.item()
+            
+            train_loss = train_loss/len(train_loader)
+            print('Epoch: {} \tTraining Loss: {:.6f}'.format(epoch, train_loss))
+    torch.save(G, 'Generator.pth')
+    print('Model saved.')
 
 def main():
-    # dataset = ResImageDataset("Images")
-
-    # fig = plt.figure()
-
-    # for i in range(len(dataset)):
-    #     sample = dataset[i]
-    #     raw = sample["raw"]
-    #     inpainted = sample["inpainted"]
-
-    #     ax = plt.subplot(1, 8, 2 * i + 1)
-    #     plt.tight_layout()
-    #     ax.set_title('Raw №{}'.format(i))
-    #     ax.axis('off')
-    #     plt.imshow(raw)
-    #     plt.pause(0.001)
-        
-    #     ax = plt.subplot(1, 8, 2 * i + 2)
-    #     plt.tight_layout()
-    #     ax.set_title('Inpainted №{}'.format(i))
-    #     ax.axis('off')
-    #     plt.imshow(inpainted)
-    #     plt.pause(0.001)
-
-    #     if i == 3:
-    #         plt.show()
-    #         break
-
-    # return
+    global G
     if os.path.isfile("Generator.pth"):
         try:
             G = torch.load('Generator.pth', map_location=device)
-            samples = torch.randn(batch_size, 128).to(device=device)
-            noise = (torch.rand(batch_size, 128) - 0.5) / 0.5
-            noise = noise.to(device)
-            fake_label_to_generate = torch.LongTensor([5 for i in range(32)]).to(device)
-            generated_samples = G(samples, fake_label_to_generate)
-            generated_samples = generated_samples.cpu().detach()
-            for i in range(25):
-                ax = plt.subplot(5, 5, i + 1)
-                plt.imshow(generated_samples[i].reshape(28, 28), cmap="gray_r")
-                plt.xticks([])
-                plt.yticks([])
+
+            fig = plt.figure()
+
+            img_index = random.randint(0, len(train_set) - 1)
+
+            img_input = train_set[img_index]["raw"]
+            shape = img_input.shape
+            img_input = img_input.view(1, shape[0], shape[1], shape[2]).to(device, dtype = torch.float)
+            print(img_input.shape)
+
+            img_output = G(img_input)
+            transform_tmp = transforms.ToPILImage()
+            img_output = img_output.view(3, img_output.shape[2], img_output.shape[3]).cpu().detach()
+            img_output = transform_tmp(img_output)
+            img_input = img_input.view(3, img_input.shape[2], img_input.shape[3]).cpu().detach()
+            img_input = transform_tmp(img_input)
+
+            img_inpainted = train_set[img_index]["inpainted"]
+            img_inpainted = img_inpainted.cpu().detach()
+            img_inpainted = transform_tmp(img_inpainted)
+
+            print(type(img_output))
+            #print(img_output.shape)
+
+            ax = plt.subplot(1, 3, 1)
+
+            plt.tight_layout()
+            ax.set_title("Input")
+            ax.axis('off')
+            plt.imshow(img_input)
+            plt.pause(0.001)
+
+            ax = plt.subplot(1, 3, 2)
+
+            ax.set_title("Output")
+            ax.axis('off')
+            plt.imshow(img_output)
+            plt.pause(0.001)
+
+            ax = plt.subplot(1, 3, 3)
+
+            ax.set_title("Inpainted")
+            ax.axis('off')
+            plt.imshow(img_inpainted)
+            plt.pause(0.001)
             plt.show()
-        except:
-            train()
+
+        except Exception as ex:
+            print(ex)
+            return
     else:
-        train()
+        print("Else")
+        #dataset = ResImageDataset("Images", transform=transform)
+        test_train()
 
 
 if __name__ == "__main__":
